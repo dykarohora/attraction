43597e22bfee31d9ed08b69718e7375ba429e084
d_yama <d_yama@hololab.co.jp>
2020-12-26 17:30:14 +0900

    WRAM構造体を定義した

--- src/nes/cpu.rs
+++ src/nes/cpu.rs
@@ -1,5 +1,10 @@
 pub struct Cpu {
+    a: u8,
     x: u8,
+    y: u8,
+    pc: u16,
+    sp: u8,
+    p: u8
 }
 
 pub fn print() {
--- src/nes/ram.rs
+++ src/nes/ram.rs
@@ -1,3 +1,11 @@
-struct Ram {
-    
+pub struct Ram {
+    memory: Vec<u8>
+}
+
+impl Ram {
+    pub fn new() -> Ram {
+        Ram {
+            memory: vec![0; 2048]
+        }
+    }
 }
