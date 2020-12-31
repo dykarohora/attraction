200258e205bcda47d41134712ffca3351bae3a8a
d_yama <7d.yama@gmail.com>
2020-12-31 23:37:11 +0900

    [update]ネームテーブルへの書き込み処理を実装した

--- src/nes/ppu.rs
+++ src/nes/ppu.rs
@@ -2,16 +2,16 @@ use crate::nes::ppu_bus::PpuBus;
 use std::cell::{Cell, RefCell};
 
 pub struct Ppu {
-    bus: PpuBus,
+    bus: RefCell<PpuBus>,
     background_palette: RefCell<Vec<u8>>,
     ppu_addr: Cell<u16>,
     is_set_ppu_high_address: Cell<bool>,
 }
 
 impl Ppu {
-    pub fn new(bus: PpuBus) -> Ppu {
+    pub fn new(ppu_bus: PpuBus) -> Ppu {
         Ppu {
-            bus,
+            bus: RefCell::new(ppu_bus),
             background_palette: RefCell::new(vec![0; 16]),
             ppu_addr: Cell::new(0x0000),
             is_set_ppu_high_address: Cell::new(false),
@@ -67,12 +67,7 @@ impl Ppu {
         // PPU ADDRによって
         let ppu_address = self.ppu_addr.get();
         match ppu_address {
-            0x2000..=0x23BF => {
-                // ネームテーブルへの書き込み
-            },
-            0x23C0..=0x23FF => {
-                // 属性テーブルへの書き込み
-            },
+            0x2000..=0x23FF => self.bus.borrow_mut().write_byte(ppu_address, byte),
             0x3F00..=0x3F0F => self.write_background_palette(ppu_address, byte),
             _ => panic!("[Ppu] not implemented or invalid address: {:#06X} byte: {:#04X}", ppu_address, byte)
         }
--- src/nes/ppu_bus.rs
+++ src/nes/ppu_bus.rs
@@ -17,6 +17,7 @@ impl PpuBus {
 
     pub fn write_byte(&mut self, address: u16, byte: u8) {
         match address {
+            0x2000..=0x23FF => self.vram.write_byte(address - 0x2000, byte),
             _ => panic!("[PPU BUS] not implemented for write_byte address:{:#06X}", address)
         }
     }
