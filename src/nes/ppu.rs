5fe0e85d8fb5463b7085f6b23c59438bde14956c
d_yama <7d.yama@gmail.com>
2020-12-31 23:20:30 +0900

    [update]パレットテーブルへの書き込み処理を実装した

--- src/nes/ppu.rs
+++ src/nes/ppu.rs
@@ -1,8 +1,9 @@
 use crate::nes::ppu_bus::PpuBus;
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 
 pub struct Ppu {
     bus: PpuBus,
+    background_palette: RefCell<Vec<u8>>,
     ppu_addr: Cell<u16>,
     is_set_ppu_high_address: Cell<bool>,
 }
@@ -11,6 +12,7 @@ impl Ppu {
     pub fn new(bus: PpuBus) -> Ppu {
         Ppu {
             bus,
+            background_palette: RefCell::new(vec![0; 16]),
             ppu_addr: Cell::new(0x0000),
             is_set_ppu_high_address: Cell::new(false),
         }
@@ -43,7 +45,7 @@ impl Ppu {
             0x2004 => (),
             0x2005 => (),
             0x2006 => self.write_ppu_addr(byte),
-            0x2007 => (),
+            0x2007 => self.write_ppu_data(byte),
             _ => panic!("[Ppu] invalid address: {:#06X}", address)
         };
         println!("[Ppu] Call write_ppu: address {:#06X}, byte {:#06X}", address, byte);
@@ -59,5 +61,27 @@ impl Ppu {
             self.ppu_addr.set((byte as u16) << 8);
             self.is_set_ppu_high_address.set(true);
         }
+    }
+
+    fn write_ppu_data(&self, byte: u8) {
+        // PPU ADDRによって
+        let ppu_address = self.ppu_addr.get();
+        match ppu_address {
+            0x2000..=0x23BF => {
+                // ネームテーブルへの書き込み
+            },
+            0x23C0..=0x23FF => {
+                // 属性テーブルへの書き込み
+            },
+            0x3F00..=0x3F0F => self.write_background_palette(ppu_address, byte),
+            _ => panic!("[Ppu] not implemented or invalid address: {:#06X} byte: {:#04X}", ppu_address, byte)
+        }
+        // PPUDATAに書き込みが発生するとPPUADDRがインクリメントされる
+        self.ppu_addr.set(ppu_address + 1);
+    }
+
+    // バックグラウンドパレットへの書き込み
+    fn write_background_palette(&self, address: u16, byte: u8) {
+        self.background_palette.borrow_mut()[(address - 0x3F00) as usize] = byte;
     }
 }
